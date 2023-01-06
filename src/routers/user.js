8249dfebfc6f37824a7de7804a1a1c48765a56ed
Rajdeep <rajdeepmallick999@gmail.com>
2023-01-06 20:41:46 +0530

    multiple events added and responsiveness of other pages and deleting docs etc added

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 const auth = require('../middleware/auth')
 const cookieParser = require('cookie-parser')
 const multer = require('multer')
+const moment = require('moment')
 
 
 const User = require('../models/userSch')
 const Clinic = require('../models/clinicSch')
 const Document = require('../models/docSch')
+const { render } = require('ejs')
 
 
 router.use(express.json())
@@ -16,26 +18,13 @@ router.use(bodyParser.urlencoded({ extended: true }))
 router.use(bodyParser.json())
 router.use(cookieParser())
 
+//  --- *** USER related all the pages will be handled here ***---
 
-router.get('/docs/:id', async (req, res) => {
-    try{
-        const id = req.params.id
-        // console.log(id)
+//  ----------/////// ******* DASHBOARD ******** \\\\\\\ ----------
 
-        const pic = await Document.findById(id)
-
-        res.set("Content-Type","image/jpg");
-        
-        res.send(pic.file)
-
-    }catch(error) {
-        console.log(error)
-    }
-})  
-
-// USER related all the pages will be handled here
-
+// --- *** Show the dashboard *** ----
 router.get('/dashboard', auth, async (req, res) => {
+
     try {
         const person_id = req.user._id
 
@@ -44,13 +33,20 @@ router.get('/dashboard', auth, async (req, res) => {
         res.render('dashboard', {
             username: req.user.username,
             documents: all_docs,
+            moment: moment
         })
     }
 
     catch (err) {
-        res.status(400).send(err)
+        // res.status(400).send(err)
+        res.render('error', {
+            reason: 'Dashboard Render Error'
+        })
     }
 })
+
+
+//  ----------/////// ******* CLINICS ******** \\\\\\\ ----------
 
 router.get('/clinics', auth, async (req, res) => {
     try {
@@ -65,21 +61,15 @@ router.get('/clinics', auth, async (req, res) => {
     }
 
     catch (err) {
-        res.status(400).send(err)
+        // res.status(400).send(err)
+        res.render('error', {
+            reason: 'Clinics Render Error'
+        })
     }
 })
 
-router.get('/docadd', auth, (req, res) => {
-    res.render('docadd', {
-        username: req.user.username
-    })
-})
 
-router.get('/clinicadd', auth, (req, res) => {
-    res.render('clinicadd', {
-        username: req.user.username
-    })
-})
+//  ----------/////// ******* SIGN IN, OUT, LOG-OUT ******** \\\\\\\ ----------
 
 
 //  ----****** Sign Up User ******----
@@ -105,7 +95,11 @@ router.post('/signup', async (req, res) => {
 
     catch (err) {
         // console.log('Error found while sign up' + err)
-        res.status(400).send(err)
+        // res.status(400).send(err)
+        // res.render('error')
+        res.render('error', {
+            reason: 'Error in SignUp'
+        })
     }
 })
 
@@ -130,15 +124,14 @@ router.post('/signin', async (req, res) => {
     }
 
     catch (error) {
-        // console.log(req.body)
-        res.status(400).send('Sahi se Details dee bhai')
+        res.render('page404')
     }
 })
 
 
 // *********** Log Out User **************
 router.get('/logout', auth, async (req, res) => {
-    try{
+    try {
 
         // log out from all devices
         req.user.tokens = []
@@ -152,12 +145,19 @@ router.get('/logout', auth, async (req, res) => {
         res.status(200).render('home')
 
     }
-    catch(error){
-        res.status(500).send(error)
+    catch (error) {
+        // res.status(500).send(error)
+        // res.render('error')
+        // res.render(404)
+        res.render('error', {
+            reason: 'Error in Logging Out'
+        })
     }
 })
 
 
+
+//  ----------/////// ******* MULTER SETUP ******** \\\\\\\ ----------
 
 
 // ---- setting up multer storage and call-back function -----
@@ -167,35 +167,34 @@ const upload = multer({
 
     limits: 100000,
 
-    fileFilter(req, file, cb){
+    fileFilter(req, file, cb) {
 
-        if(file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|PNG|png)$/)){
+        if (file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|PNG|png)$/)) {
             cb(undefined, true)
         }
 
-        else{
+        else {
             cb(new Error("Please upload only .jpg file"))
-            cb(undefined,false)
+            cb(undefined, false)
         }
     },
-    
+
     storage
 })
 
 
+//  ----------/////// ******* DOCUMENTS ADDING PAGES ******** \\\\\\\ ----------
+
+router.get('/docadd', auth, (req, res) => {
+    res.render('docadd', {
+        username: req.user.username
+    })
+})
+
 router.post('/docadd', auth, upload.single('Prescription'), async (req, res) => {
 
     try {
-        // console.log(req.user)
-
-        // console.log(JSON.parse(JSON.stringify(req.body)))
-
-        // console.log(req)
-
         const obj = JSON.parse(JSON.stringify(req.body))
-
-        // console.log(obj)
-        // console.log(req.file)
 
         // req.document 
         req.user.file = req.file.buffer
@@ -216,10 +215,68 @@ router.post('/docadd', auth, upload.single('Prescription'), async (req, res) => 
     }
 
     catch (error) {
-        res.status(400).send(error)
+        // res.render('error')
+        // res.status(400).send(error)
+        res.render('error', {
+            reason: 'Error in Adding Document'
+        })
     }
 })
 
+// -----**** Show the prescription ****----
+router.get('/docs/:id', async (req, res) => {
+    try {
+        const _id = req.params.id
+
+        const pic = await Document.findById(_id)
+
+        res.set("Content-Type", "image/jpg");
+
+        res.send(pic.file)
+
+    } catch (error) {
+        // console.log(error)
+        res.render('page404')
+    }
+})
+
+// -----**** Delete the prescription ****----
+router.post('/docs/delete/:id', auth, async (req, res) => {
+    // console.log(req.params.id)
+
+    try {
+        const _id = req.params.id
+
+        const doc = await Document.findByIdAndDelete(_id)
+
+        if (!doc)
+            return res.status(404).send()
+
+        const person_id = req.user._id
+
+        const all_docs = await Document.find({ "person_id": person_id })
+
+        res.redirect('../../dashboard', {
+            username: req.user.username,
+            documents: all_docs,
+            moment: moment
+        }, 201)
+
+    } catch (err) {
+        res.render('error', {
+            reason: 'Error in Deleting Document'
+        })
+    }
+})
+
+
+//  ----------/////// ******* CLINICS ADDING & DELETING PAGES ******** \\\\\\\ ----------
+
+router.get('/clinicadd', auth, (req, res) => {
+    res.render('clinicadd', {
+        username: req.user.username
+    })
+})
 
 router.post('/clinicadd', auth, async (req, res) => {
 
@@ -240,9 +297,47 @@ router.post('/clinicadd', auth, async (req, res) => {
 
     catch (error) {
         console.log(error)
-        res.status(400).send(error)
+        // res.status(400).send(error)
+        res.render('error')
     }
 })
 
+router.post('/clinics/delete/:id', auth, async (req, res) => {
+    // console.log(req.params.id)
+
+    try {
+
+        const id = req.params.id
+
+        const clinic = await Clinic.findByIdAndDelete(id)
+
+        // console.log(clinic)
+        if (!clinic)
+            return res.status(404).send()
+
+        // res.render('clinics')
+        const person_id = req.user._id
+
+        // console.log(person_id)
+
+        const all_clinics = await Clinic.find({ "person_id": person_id })
+
+        res.redirect('../../clinics', {
+            username: req.user.username,
+            clinics: all_clinics,
+        }, 201)
+
+    } catch (err) {
+        // console.log(err)
+        // res.status(500).send(err)
+        res.render('error')
+    }
+
+})
+
+
+// ----**** Delete a particular Clinic *** -----
+// we can't make a delete request using pure html
+// any request in this route will be treated as a delete request
 
 module.exports = router
